10149261978e13380ba1b7e83bc0c12752576080
grigorisem <96693189+grigorisem@users.noreply.github.com>
2023-06-19 17:55:12 +0600

    Delete gym directory

--- gym/authapp/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Enrollment, MembershipPlan
-
-# Register your models here.
-admin.site.register(Enrollment)
-admin.site.register(MembershipPlan)
